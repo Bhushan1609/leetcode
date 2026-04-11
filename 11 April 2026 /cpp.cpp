cfc7446b59e460ba57a14e1b914a3537cec6fc0e
Bhushan Thombare <bhushan.thombare.cs1609@gmail.com>
2026-04-11 23:11:53 +0530

    Update cpp.cpp

--- 11 April 2026 /cpp.cpp
+++ 11 April 2026 /cpp.cpp
@@ -12,7 +12,7 @@ using u64=unsigned long long;
 
 template <class T,class P>
 using pi=pair<T,P>;
-
+ 
 #define INT(...)int __VA_ARGS__; IN(__VA_ARGS__)
 #define LL(...)ll __VA_ARGS__; IN(__VA_ARGS__)
 #define STR(...)string __VA_ARGS__; IN(__VA_ARGS__)
